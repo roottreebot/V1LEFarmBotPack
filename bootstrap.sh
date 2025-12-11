c7fde6950a7715d1e9d5550472c273ef5f85ca42
roottreebot <v1lefarm.net@gmail.com>
2025-12-11 10:56:27 +1030

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Set your bot token & admin IDs before running
+export BOT_TOKEN="YOUR_BOT_TOKEN_HERE"
+export ADMIN_IDS="6665237815,8031966101"
+
+bash -c "$(curl -s https://raw.githubusercontent.com/roottreebot/V1LEFarmBotPack/refs/heads/main/install_bot.sh)"
