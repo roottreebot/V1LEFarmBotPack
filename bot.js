02d02f456dccd139a44daa402cf8b34a24db1749
RootTree <v1lefarm.net@gmail.com>
2025-12-14 01:13:59 +1030

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,13 +110,10 @@ function xpBar(xp, lvl) {
 
 // ================= MOBILE-FRIENDLY ASCII =================
 const HEADER = `
-\`\`\`
-█V   V  1  L     EEEEE 
-V   V 11  L     E     
-V   V  1  L     EEEE  
- V V   1  L     E     
-  V   111 LLLLL EEEEE 
-                                         
+\`\`\`                                                   
+█████▄   ▄██▄   ▄██▄ ██████ 
+██▄▄██▄ ██  ██ ██  ██  ██   
+██   ██  ▀██▀   ▀██▀   ██                                        
         V 1 L E   F A R M
 \`\`\`
 `;
