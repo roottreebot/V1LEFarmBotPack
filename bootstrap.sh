e64098e451202820b005d524b07b91895ee9f48e
RootTree <v1lefarm.net@gmail.com>
2025-12-14 13:29:55 +1030

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,12 @@ set -e
 
 pkg update -y || apt update -y
 pkg upgrade -y || apt upgrade -y
-
 pkg install -y nodejs git curl || apt install -y nodejs git curl
 npm install -g pm2
 
 cd ~
-rm -rf v1lefarm
-git clone https://github.com/roottreebot/V1LEFarmBotPack.git v1lefarm
-cd v1lefarm
+rm -rf V1LEFarmBot
+git clone https://github.com/YOUR_USERNAME/V1LEFarmBot.git
+cd V1LEFarmBot
 
 bash install_bot.sh
