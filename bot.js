3cd8a2260bcfa77d1eb5335f3242509277676e7b
RootTree <v1lefarm.net@gmail.com>
2025-12-21 23:27:46 +1030

    Update bot.js

--- bot.js
+++ bot.js
@@ -292,7 +292,7 @@ bot.on('message', msg => {
 
   const s = sessions[id];
   if (!s || s.step !== 'amount' || (s.lockedUntil && Date.now() < s.lockedUntil)) return;
-  s.lockedUntil = Date.now() + 2_000; // lock 30s
+  s.lockedUntil = Date.now() + 02_000; // lock 30s
 
   const price = PRODUCTS[s.product].price;
   let grams, cash;
