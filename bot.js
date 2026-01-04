32b82328c7a7370318e53dbd863e8822ea05fb2d
RootTree <v1lefarm.net@gmail.com>
2026-01-04 11:18:46 +1030

    FIX (FINAL: V.2.00.25 )

--- bot.js
+++ bot.js
@@ -311,8 +311,8 @@ async function showMainMenu(id, lbPage = 0) {
       const isBulk = parseFloat(o.cash) >= 400;
 
       return isBulk
-        ? `${o.status === '✅ Accepted' ? '▏🟢' : '▏⚪'} *${o.product}* — 🧱 *Bulk Order* — *${o.status}*`
-        : `${o.status === '✅ Accepted' ? '▏🟢' : '▏⚪'} *${o.product}* — ${o.grams}g — $${o.cash} — *${o.status}*`;
+        ? `${o.status === '✅' ? '▏🟢' : '▏⚪'} *${o.product}* — 🧱 *Bulk Order* — *${o.status}*`
+        : `${o.status === '✅' ? '▏🟢' : '▏⚪'} *${o.product}* — ${o.grams}g — $${o.cash} — *${o.status}*`;
     }).join('\n')
   : '_No orders yet_';
 
